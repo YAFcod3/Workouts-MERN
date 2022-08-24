311e50e8b8de0465f0666aee3a1dd1434c7d0673
Yasmani Armas <yasmaniarmas1995@gmail.com>
2022-08-24 19:59:17 -0400

    condicional en navbar y autent persistente con effect ok

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
 import {useLogout} from '../hooks/useLogout'
+import { useAuthContext } from "../hooks/useAuthContext";
+
 
 
 const Navbar = () => {
 
     const {logout} = useLogout()
+    const {user}=useAuthContext()
+
    
     //CLICK
     const handleClick =()=>{
@@ -21,14 +25,18 @@ const Navbar = () => {
                 <Link to='/'><h1>Workout Buddy</h1></Link>
 
                 <nav>
-                    <div>
-                      <button onClick={handleClick}>Logout</button>
-                    </div>
-                    <div>
-                        <Link to='/login'>Login</Link>
-                        <Link to='/signup'>Signup</Link>
-                    </div>
-               
+                    {user && (
+                        <div>
+                        <span>{user.email}</span>
+                        <button onClick={handleClick}>Logout</button>
+                        </div>
+                    )}
+                    {!user && (
+                        <div>
+                            <Link to='/login'>Login</Link>
+                            <Link to='/signup'>Signup</Link>
+                        </div>
+                    )}
                 </nav>
                 
             </div>
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext,useReducer } from "react";
+import { createContext,useEffect,useReducer } from "react";
 
 
 export const AuthContext= createContext()
@@ -23,6 +23,17 @@ const authReducer = (state,action)=>{
 export const AuthContextProvider = ({children})=>{
 
     const [state,dispatch ]=useReducer(authReducer,{user:null})
+
+    //add auth persistente
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user'))
+
+        if (user){
+            dispatch({type:'LOGIN', payload:user})
+        }
+    }, [])
+
+    
 
     console.log('AuthContext state',state)
 
