453c56da2c43d5d78693e1e5a3dfdc9c4224b4c7
Yasmani Armas <yasmaniarmas1995@gmail.com>
2022-08-25 00:27:55 -0400

    rutas privadas ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-
+import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext';
 
 //pages & components
 import Home from './pages/Home';
@@ -10,6 +10,8 @@ import Signup from './pages/Signup';
 
 
 function App() {
+  
+  const {user}=useAuthContext()
 
 
   
@@ -19,9 +21,9 @@ function App() {
         <Navbar/>
         <div className='pages'>
           <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/signup' element={<Signup/>}/>
+            <Route path='/' element={ user ?<Home/> :<Navigate to='/login'/>}/>
+            <Route path='/login' element={!user ?<Login/> :<Navigate to='/'/>}/>
+            <Route path='/signup' element={!user ?<Signup/> :<Navigate to='/'/>}/> 
           
           </Routes>
         
